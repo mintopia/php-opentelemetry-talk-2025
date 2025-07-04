634d97d7161811de5f7b7da49680782e46c61338
Jessica Smith <jessica.smith@digicert.com>
2025-07-04 17:35:57 +0100

    Updates after testing

--- code/app/Http/Controllers/QuoteController.php
+++ code/app/Http/Controllers/QuoteController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Quote;
+use Illuminate\Support\Facades\Log;
 
 class QuoteController extends Controller
 {
     public function home()
     {
         $quote = Quote::inRandomOrder()->first();
+        Log::debug("Retrieving random quote with ID: {$quote->id}");
         return view('quote', [
             'quote' => $quote,
         ]);
+    }
+
+    public function error()
+    {
+        throw new \Exception('This is an unexpected error');
     }
 }
--- code/routes/web.php
+++ code/routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\QuoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [QuoteController::class, 'home'])->name('home');
+Route::get('/error', [QuoteController::class, 'error'])->name('error');
